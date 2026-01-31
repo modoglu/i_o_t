cf5505b4d6e07d74c4c7d14b4e00ad62ff5ed794
Enes Mahmut Ates <eates@k2m17s09.42kocaeli.com.tr>
2026-01-31 16:30:56 +0300

    iot

--- p1/scripts/server_setup.sh
+++ p1/scripts/server_setup.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 set -e 
 
-echo "--SERVER SETUP START--"
+echo "-- SERVER SETUP START --"
 
-# Gerekli paketlerin kurulumu
 sudo apt-get update
-sudo apt-get install curl -y
+sudo apt-get install -y curl
+sudo apt-get install -y net-tools
 
-# K3s Kurulumu
-# --write-kubeconfig-mode: kubectl'in sudo olmadan çalışmasını sağlar
-# --disable traefik: 512MB RAM için hayat kurtarır
-curl -sfL https://get.k3s.io | sh -s - server \
-    --node-ip=192.168.56.110 \
-    --flannel-iface=eth1 \
-    --write-kubeconfig-mode 644 \
-    --disable traefik
+export INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode 644 --disable traefik"
+curl -sfL https://get.k3s.io | sh -
 
-# Token oluşana kadar bekle
-echo "Waiting for node-token..."
-until [ -f /var/lib/rancher/k3s/server/node-token ]; do
-  sleep 2
+while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
+  sleep 1
 done
 
-# Token'ı paylaş
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+echo "export PATH=\$PATH:/sbin" >> /home/vagrant/.bashrc
+
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
 sudo chmod 644 /vagrant/node-token
 
-echo "--SERVER SETUP COMPLETE--"
+echo "-- SERVER SETUP COMPLETE --"
--- p1/scripts/worker_setup.sh
+++ p1/scripts/worker_setup.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 set -e 
 
-echo "--WORKER SETUP START--"
+echo "-- WORKER SETUP START --"
 
-# Gerekli paketlerin kurulumu
 sudo apt-get update
-sudo apt-get install curl -y
+sudo apt-get install -y curl
 
-# Token dosyasının sunucudan gelmesini bekle (Race Condition engelleme)
-echo "Waiting for node-token from server..."
-while [ ! -f /vagrant/node-token ]; do
-  sleep 2
+while [ ! -r /vagrant/node-token ]; do
+  sleep 1
 done
 
-# K3s Agent Kurulumu
-# K3S_URL: Sunucu IP'si (Dokümandaki 110 IP'si)
-# K3S_TOKEN: Paylaşılan dosyadan okunan token
 export K3S_URL="https://192.168.56.110:6443"
 export K3S_TOKEN=$(cat /vagrant/node-token)
+export INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1"
 
-curl -sfL https://get.k3s.io | K3S_URL=${K3S_URL} K3S_TOKEN=${K3S_TOKEN} sh -s - --node-ip=192.168.56.111 --flannel-iface=eth1
+curl -sfL https://get.k3s.io | sh -
 
-echo "--WORKER SETUP COMPLETE--"
+echo "-- WORKER SETUP COMPLETE --"
