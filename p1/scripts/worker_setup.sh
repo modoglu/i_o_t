568a0575244ea653f2b3b4f6cf43d63f203ce01b
Enes Mahmut Ates <eates@k2m17s06.42kocaeli.com.tr>
2026-02-02 18:12:31 +0300

    iot

--- p1/scripts/worker_setup.sh
+++ p1/scripts/worker_setup.sh
@@ -5,8 +5,9 @@ echo "-- WORKER SETUP START --"
 
 sudo apt-get update
 sudo apt-get install -y curl
+sudo apt-get install -y net-tools
 
-while [ ! -r /vagrant/node-token ]; do
+until [ -r /vagrant/node-token ]; do
   sleep 1
 done
 
@@ -15,5 +16,8 @@ export K3S_TOKEN=$(cat /vagrant/node-token)
 export INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1"
 
 curl -sfL https://get.k3s.io | sh -
+
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+echo "export PATH=\$PATH:/sbin" >> /home/vagrant/.bashrc
 
 echo "-- WORKER SETUP COMPLETE --"
--- p2/scripts/server_setup.sh
+++ p2/scripts/server_setup.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y curl
 sudo apt-get install -y net-tools
 
-export INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode 644 --disable traefik"
+export INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode 644"
 curl -sfL https://get.k3s.io | sh -
 
 until sudo kubectl get nodes; do
@@ -19,5 +19,10 @@ echo "export PATH=\$PATH:/sbin" >> /home/vagrant/.bashrc
 
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
 sudo chmod 644 /vagrant/node-token
+
+sudo kubectl apply -f /vagrant/confs/app1.yaml
+sudo kubectl apply -f /vagrant/confs/app2.yaml
+sudo kubectl apply -f /vagrant/confs/app3.yaml
+sudo kubectl apply -f /vagrant/confs/ingress.yaml
 
 echo "-- SERVER SETUP COMPLETE --"
